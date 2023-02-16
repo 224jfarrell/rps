7bc3f5886266687275aa7c67a3779cd2f4c41b10
224jfarrell <224jfarrell@frhsd.com>
2023-02-16 09:31:04 -0500

    ok

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function disable(){
+document.addEventListener('DOMContentLoaded', function(event) {
     document.getElementById('rock2').disabled = true;
     document.getElementById('paper2').disabled = true;
     document.getElementById('scissors2').disabled = true;
-    document.getElementById('player2').disabled = true;
+    document.getElementById('player2').hidden = true;
+})
 
 function lockInUser(answer){
     document.getElementById('choiceUser').append(answer)
@@ -40,22 +41,49 @@ function lockInUser(answer){
     }
 }
 
-// function lockInP1(answer){
-//     document.getElementById('choiceP1').append("?????")
-//     document.getElementById('rock1').disabled = true;
-//     document.getElementById('paper1').disabled = true;
-//     document.getElementById('scissors1').disabled = true;
-//     document.getElementById('player1').disabled = true;
-//     document.getElementById('rock2').disabled = false;
-//     document.getElementById('paper2').disabled = false;
-//     document.getElementById('scissors2').disabled = false;
-//     document.getElementById('player2').disabled = false;
-// }
 
-// function lockInP2(answer){
-//     document.getElementById('choiceP2').append("?????")
-//     document.getElementById('rock2').disabled = true;
-//     document.getElementById('paper2').disabled = true;
-//     document.getElementById('scissors2').disabled = true;
-//     document.getElementById('player2').disabled = true;
-// }
+function lockInP1(){
+    document.getElementById('choiceP1').append("?????")
+    document.getElementById('rock1').disabled = true;
+    document.getElementById('paper1').disabled = true;
+    document.getElementById('scissors1').disabled = true;
+    document.getElementById('player1').hidden = true;
+    document.getElementById('rock2').disabled = false;
+    document.getElementById('paper2').disabled = false;
+    document.getElementById('scissors2').disabled = false;
+    document.getElementById('player2').hidden = false;
+}
+
+function lockInP2(){
+    document.getElementById('choiceP2').append("?????")
+    document.getElementById('rock2').disabled = true;
+    document.getElementById('paper2').disabled = true;
+    document.getElementById('scissors2').disabled = true;
+    document.getElementById('player2').hidden = true;
+}
+
+function userInputs(answer1, answer2){
+    if(answer2){
+        if(answer1 == 'rock' && answer2 == 'scissors'){
+            document.getElementById('result').append('p1 win');
+        } else if(answer1 == 'paper' && answer2 == 'rock'){
+            document.getElementById('result').append('p1 win');
+        } else if(answer1 == 'scissors' && answer2 == 'paper'){
+            document.getElementById('result').append('p1 win');
+        } else if(answer1 == 'rock' && answer2 == 'rock'){
+            document.getElementById('result').append('tie');
+        } else if(answer1 == 'paper' && answer2 == 'paper'){
+            document.getElementById('result').append('tie');
+        } else if(answer1 == 'scissors' && answer2 == 'scissors'){
+            document.getElementById('result').append('tie');
+        } else if(answer1 == 'rock' && answer2 == 'paper'){
+            document.getElementById('result').append('p2 win');
+        } else if(answer1 == 'paper' && answer2 == 'scissors'){
+            document.getElementById('result').append('p2 win');
+        } else if(answer1 == 'scissors' && answer2 == 'rock'){
+            document.getElementById('result').append('p2 win');
+        }
+    }
+    console.log(answer1);
+    console.log(answer2);
+}
